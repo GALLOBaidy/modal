015478d883fd0a17d44bfbbc52ba32d5a6e94e9b
Baïdy Gallo <ba.gal@dwwm.evx>
2024-11-20 11:00:51 +0000

    les commentaires

--- js/app.js
+++ js/app.js
@@ -3,17 +3,27 @@
 // quand un utilisateur 'click' sur modal-btn ajouter .open-modal à modal-overlay
 // quand un utilisateur 'click' sur close-btn supprimer .open-modal de modal-overlay
 
+
+
+//On crée nos variable pour pouvoir cibler nos balises button grâce à leur class
 let _open = document.querySelector(".modal-btn");
 let _modal = document.querySelector(".modal-overlay");
 let _close = document.querySelector(".close-btn");
 
+//On ajoute un événement click sur du button "open modal"
 _open.addEventListener("click", ouvrir);
 
+//La fonction s'éxécute dès que l'événement est déclenché
+//Ajout de la class "open-modal"
 function ouvrir(){
     _modal.classList.add("open-modal");
 }
 
+//On ajoute un événement click sur du button qui a la class "close-btn"
 _close.addEventListener("click", fermer);
+
+//La fonction s'éxécute dès que l'événement est déclenché
+//On retire la class "open-modal"
 function fermer(){
     _modal.classList.remove("open-modal");
 }
